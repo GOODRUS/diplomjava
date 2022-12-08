32a0b40517b4585e1f63a5a2b0b060148dd33d20
Evgenii Rybalkin <eugeny.rybalkin@gmail.com>
2022-12-08 19:29:37 +0100

    add new tests

--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -16,8 +16,8 @@ public class PlayerTest {
         player.installGame(NFS);
 
         int expected = 3;
-        int actual = player.play(NFS,3);
-        assertEquals(expected,actual);
+        int actual = player.play(NFS, 3);
+        assertEquals(expected, actual);
     }
 
 //    @Test
@@ -38,32 +38,56 @@ public class PlayerTest {
 //        assertTrue(thrown);
 //    }
 
-//    @Test
-//    public void shouldSumPlayTimeIfGamesHaveTheSameGenre() {
-//        Player player = new Player("Petya");
-//        GameStore store = new GameStore();
-//        Game game = store.publishGame("Test Drive", "racing");
-//        Game game2 = store.publishGame("NFS", "racing");
-//        Game game3 = store.publishGame("Forza5", "racing");
-//        Game game4 = store.publishGame("Fly", "simulator");
-//        Game game5 = store.publishGame("COLD", "action");
-//        Game game6 = store.publishGame("MadMax", "action");
-//
-//        player.installGame(game);
-//        player.installGame(game2);
-//        player.installGame(game3);
-//        player.installGame(game5);
-//
-//        player.play(game, 30);
-//        player.play(game2, 10);// метод sumGenre суммирует время игры , только до тех пор, пока идут игры одного жанра.
-//        player.play(game3, 60);
-//
-//        player.play(game4, 5);
-//        player.play(game5, 2);
-//        player.play(game6, 2);
-//
-//        int expected = 100;
-//        int actual = player.sumGenre("racing");
-//        assertEquals(expected, actual);
-//    }
+
+
+
+    @Test    // Почему тест падает если запустить его , но проходит в режиме отладки ??
+    public void shouldSumPlayTimeIfGamesHaveTheSameGenre() {
+        Player player = new Player("Petya");
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Test Drive", "racing");
+        Game game2 = store.publishGame("NFS", "racing");
+        Game game3 = store.publishGame("Forza5", "racing");
+        Game game4 = store.publishGame("Fly", "action");
+        Game game5 = store.publishGame("Fly2", "simulator");
+
+        player.installGame(game1);
+        player.installGame(game2);
+        player.installGame(game3);
+        player.installGame(game4);
+
+        player.play(game1, 30);
+        player.play(game3, 60);
+        player.play(game2, 10);
+        player.play(game4, 50);
+        player.play(game5, 20);
+
+        int expected = 20;
+        int actual = player.sumGenre("simulator");
+        assertEquals(expected, actual);
+    }
+
+
+
+    @Test // тест актуален после исправления метода "Play"
+    public void shouldNotRewriteGameTimeIfTheGameWillReinstall() {
+        GameStore store = new GameStore();
+        Player player = new Player("Petya");
+        Game game = store.publishGame("Test Drive", "simulator");
+        Game game2 = store.publishGame("NFS", "simulator");
+        Game game3 = store.publishGame("Forza5", "simulator");
+        Game game4 = store.publishGame("Fly", "simulator");
+
+        player.play(game, 25);
+        player.play(game2, 10);
+        player.play(game3, 60);
+        player.play(game4, 5);
+        player.installGame(game4);  /** устанавливаем второй раз - программа должна игнорировать это действие
+                                      и не перезаписывать время игры на 0 */
+
+        int expected = 100;
+        int actual = player.sumGenre("simulator");
+        assertEquals(expected, actual);
+    }
 }
+
