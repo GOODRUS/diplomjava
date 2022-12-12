2bd83a37c0642e965372358fcb76bef362266c1a
Dmitriy Shilyaev <goodrus1993@yandex.ru>
2022-12-12 20:49:05 +0500

    fix Player

--- src/main/java/ru/netology/Player.java
+++ src/main/java/ru/netology/Player.java
@@ -1,14 +1,17 @@
 package ru.netology;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Player {
     private String name;
 
-    /** информация о том, в какую игру сколько часов было сыграно
-    ключ - игра
-    значение - суммарное количество часов игры в эту игру */
+    /**
+     * информация о том, в какую игру сколько часов было сыграно
+     * ключ - игра
+     * значение - суммарное количество часов игры в эту игру
+     */
     private Map<Game, Integer> playedTime = new HashMap<>();
 
     public Player(String name) {
@@ -19,29 +22,47 @@ public class Player {
         return name;
     }
 
-    /** добавление игры игроку
-    если игра уже была, никаких изменений происходить не должно */
+    /**
+     * добавление игры игроку
+     * если игра уже была, никаких изменений происходить не должно
+     */
     public void installGame(Game game) {
         playedTime.put(game, 0);
     }
 
-    /** игрок играет в игру game на протяжении hours часов
-    об этом нужно сообщить объекту-каталогу игр, откуда была установлена игра
-    также надо обновить значения в мапе игрока, добавив проигранное количество часов
-    возвращает суммарное количество часов, проигранное в эту игру.
-    если игра не была установлена, то надо выкидывать RuntimeException */
-    public int play(Game game, int hours) {
-        game.getStore().addPlayTime(name, hours);
+    public void installGameAlternative(Game game) {
         if (playedTime.containsKey(game)) {
             playedTime.put(game, playedTime.get(game));
         } else {
-            playedTime.put(game, hours);
+            playedTime.put(game, 0);
+        }
+    }
+
+
+
+    /**
+     * игрок играет в игру game на протяжении hours часов
+     * об этом нужно сообщить объекту-каталогу игр, откуда была установлена игра
+     * также надо обновить значения в мапе игрока, добавив проигранное количество часов
+     * возвращает суммарное количество часов, проигранное в эту игру.
+     * если игра не была установлена, то надо выкидывать RuntimeException
+     */
+
+    public int play(Game game, int hours) {
+        game.getStore().addPlayTime(name, hours);
+        if (playedTime.containsKey(game)) {
+            playedTime.merge(game, hours, (oldValue, newValue) -> oldValue + newValue);
+        } else {
+            throw new NotInstallException(game.getTitle());
         }
         return playedTime.get(game);
     }
 
-    /** Метод принимает жанр игры (одно из полей объекта игры) и
-     суммирует время, проигранное во все игры этого жанра этим игроком */
+
+    /**
+     * Метод принимает жанр игры (одно из полей объекта игры) и
+     * суммирует время, проигранное во все игры этого жанра этим игроком
+     */
     public int sumGenre(String genre) {
         int sum = 0;
         for (Game game : playedTime.keySet()) {
@@ -54,9 +75,103 @@ public class Player {
         return sum;
     }
 
-    /** Метод принимает жанр и возвращает игру этого жанра, в которую играли больше всего
-     Если в игры этого жанра не играли, возвращается null */
+    /**
+     * Метод принимает жанр и возвращает игру этого жанра, в которую играли больше всего
+     * Если в игры этого жанра не играли, возвращается null
+     */
+
     public Game mostPlayerByGenre(String genre) {
-        return null;
+
+        ArrayList<Game> games = new ArrayList<>();
+        Game bestGame = null;
+        int mostTime = 0;
+
+        for (Game game : playedTime.keySet()) {
+            if (game.getGenre().equals(genre)) {
+                games.add(game);
+            } else {
+                continue;
+            }
+            int gamePlay = playedTime.get(game);
+            if (gamePlay >= mostTime) {
+                mostTime = gamePlay;
+                bestGame = game;
+            }
+        }
+        return bestGame;
+    }
+
+    public ArrayList<Game> mostPlayerByGenreAlternative(String genre) {
+
+        ArrayList<Game> games = new ArrayList<>();
+
+        for (Game game : playedTime.keySet()) {
+            if (game.getGenre().equals(genre)) {
+                games.add(game);
+            } else {
+                continue;
+            }
+            int mostTime = 0;
+            int gameTime = playedTime.get(game);
+            if (gameTime >= mostTime) {
+                mostTime = gameTime;
+            } else {
+                games.remove(game);
+            }
+        }
+        return games;
+    }
+
+    public ArrayList<Game> mostPlayerByGenreAlternativeOne(String genre) {
+
+        ArrayList<Game> games = new ArrayList<>();
+
+        for (Game game : playedTime.keySet()) {
+            if (game.getGenre().equals(genre)) {
+                games.add(game);
+                int mostTime = 0;
+                int gameTime = playedTime.get(game);
+                if (gameTime >= mostTime) {
+                    mostTime = gameTime;
+                } else {
+                    games.remove(game);
+                }
+            } else {
+                continue;
+            }
+            return games;
+        }
+        if (games.size() == 0) {
+            games.add(null);
+        }
+        return games;
+    }
+
+    public ArrayList<Game> mostPlayerByGenreAlternativeTwo(String genre) {
+
+        ArrayList<Game> games = new ArrayList<>();
+
+        for (Game game : playedTime.keySet()) {
+            if (game.getGenre().equals(genre)) {
+                games.add(game);
+            }
+            if (games.size() == 0) {
+                games.add(null);
+            }
+            if (games.size() > 0) {
+                games.remove(null);
+            } else {
+                continue;
+            }
+            int mostTime = 0;
+            int gamePlay = playedTime.get(game);
+            if (gamePlay >= mostTime) {
+                mostTime = gamePlay;
+
+            } else {
+                games.remove(game);
+            }
+        }
+        return games;
     }
 }
